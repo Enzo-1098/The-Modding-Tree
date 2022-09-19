ab2646a20f03dd30810ad9566ba49239e575fbcd
Enzo-1098 <113872677+Enzo-1098@users.noreply.github.com>
2022-09-19 11:25:43 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Anti-Eternity Tree",
+	id: "antieternity",
+	author: "Enzo-1098",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
